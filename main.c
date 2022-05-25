4663ac20d9c0da5491e2968e94b35e6bce9a3a48
RDW <duckwhale@mail.ru>
2022-05-25 15:02:38 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "mongoose/mongoose.h"
+
+
+static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
+  struct mg_http_serve_opts opts = {.root_dir = "."};   // Serve local dir
+  if (ev == MG_EV_HTTP_MSG) mg_http_serve_dir(c, ev_data, &opts);
+}
+
+
+int main() {
+
+  struct mg_mgr mgr;
+  mg_mgr_init(&mgr);
+  mg_http_listen(&mgr, "0.0.0.0:8000", fn, NULL);     // Create listening connection
+  for (;;) mg_mgr_poll(&mgr, 1000);                   // Block forever
+}
