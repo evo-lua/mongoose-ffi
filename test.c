abbbeccaa59656da5b6d292a8b8be5e15724c6a6
RDW <duckwhale@mail.ru>
2022-05-26 01:22:26 +0200

    tests

--- test.c
+++ test.c
@@ -40,14 +40,27 @@ static String test_bar() {
 static void TestEventQueue() {
 
 	EventQueue queue;
+	memset(&queue, 0, sizeof(EventQueue));
 
 	// Initialization
+	printf("Testing %s\n", "EventQueue_Initialize"); // implicit
 	assert(queue.firstEvent == NULL, "should initialize first element with NULL");
 	assert(queue.lastEvent == NULL, "should initialize last element with NULL");
 
-	// PushBack
+	// GetFront
+	printf("Testing %s\n", "EventQueue_GetFront");
+	assert(EventQueue_GetFront(&queue) == NULL, "should return NULL if the list is empty");
 
 	// PushBack
+	printf("Testing %s\n", "PushBack");
+
+	MongooseEvent event1;
+	memset(&event1, 0, sizeof(MongooseEvent));
+	EventQueue_PushBack(&queue, &event1);
+	assert(queue.firstEvent == &event1, "should set the first element if only one event was added");
+	assert(queue.lastEvent == &event1, "should set the last element if only one event was added");
+	assert(EventQueue_GetFront(&queue) == &event1, "should return the first element if only one was added");
+
 }
 
 static void run_all_tests() {
