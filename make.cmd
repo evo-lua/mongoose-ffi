f4ab91f9cb9554118b12346ea7adeade533238ac
RDW <duckwhale@mail.ru>
2022-05-26 22:24:11 +0200

    streamline mongoose dll creation (windows only)

--- make.cmd
+++ make.cmd
@@ -1,7 +1,7 @@
 @echo OFF
-cl /nologo /LD /O2 mongoose/mongoose.c mongoose_ffi_bindings.c -o mongoose_ffi.dll /link /DEF:mongoose_ffi.def Ws2_32.lib
-cl /nologo /O2 mongoose/mongoose.c main.c /Fe"test-mongoose.exe" /link Ws2_32.lib
-cl /nologo /O2 test.c /Fe"test.exe" /link
+cl /nologo /LD /O2 mongoose/mongoose.c -o mongoose.dll /link /DEF:mongoose.def Ws2_32.lib
+@REM cl /nologo /O2 mongoose/mongoose.c main.c /Fe"test-mongoose.exe" /link Ws2_32.lib
+@REM cl /nologo /O2 test.c /Fe"test.exe" /link
 REM cl /nologo /O2 utlist_test.c /Fe"utlist_test.exe" /link
 REM cl /nologo /O2 sys_test.c /Fe"sys_test.exe" /link
 
