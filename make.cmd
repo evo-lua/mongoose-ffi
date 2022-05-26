f24b439444a095fe727345d69d9e76e0920de36f
RDW <duckwhale@mail.ru>
2022-05-27 01:26:54 +0200

    Revert "Revert "wip openssl integration""
    This reverts commit 1d4d2b391610c8c9c96a974238ef8d179f61cbb9.

--- make.cmd
+++ make.cmd
@@ -1,5 +1,5 @@
 @echo OFF
-cl /nologo /LD /O2 /DMG_ENABLE_MD5 /DMG_ENABLE_IPV6 /DMG_ENABLE_LINES mongoose/mongoose.c -o mongoose.dll /link /DEF:mongoose.def Ws2_32.lib
+cl /nologo /LD /O2 /I include /DMG_ENABLE_MD5 /DMG_ENABLE_IPV6 /DMG_ENABLE_LINES /DMG_ENABLE_OPENSSL mongoose/mongoose.c -o mongoose.dll /link /DEF:mongoose.def Ws2_32.lib libssl.lib libcrypto.lib
 @REM cl /nologo /O2 mongoose/mongoose.c main.c /Fe"test-mongoose.exe" /link Ws2_32.lib
 @REM cl /nologo /O2 test.c /Fe"test.exe" /link
 REM cl /nologo /O2 utlist_test.c /Fe"utlist_test.exe" /link
@@ -8,4 +8,4 @@ REM cl /nologo /O2 sys_test.c /Fe"sys_test.exe" /link
 REM Some optional cleanup to save disk space and de-clutter the repository
 if exist *.obj del *.obj
 if exist *.exp del *.exp
-if exist *.lib del *.lib
+@REM if exist *.lib del *.lib
