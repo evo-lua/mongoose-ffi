e4c7778b30b75a856fc98687c39e35957794a201
RDW <duckwhale@mail.ru>
2022-05-26 23:00:18 +0200

    MG_ENABLE_IPV6

--- make.cmd
+++ make.cmd
@@ -1,5 +1,5 @@
 @echo OFF
-cl /nologo /LD /O2 /DMG_ENABLE_MD5 mongoose/mongoose.c -o mongoose.dll /link /DEF:mongoose.def Ws2_32.lib
+cl /nologo /LD /O2 /DMG_ENABLE_MD5 /DMG_ENABLE_IPV6 mongoose/mongoose.c -o mongoose.dll /link /DEF:mongoose.def Ws2_32.lib
 @REM cl /nologo /O2 mongoose/mongoose.c main.c /Fe"test-mongoose.exe" /link Ws2_32.lib
 @REM cl /nologo /O2 test.c /Fe"test.exe" /link
 REM cl /nologo /O2 utlist_test.c /Fe"utlist_test.exe" /link
