4ac97011edb00cd9d58015d51e8acee8b146d7d8
RDW <duckwhale@mail.ru>
2022-05-27 11:58:07 +0200

    fix ssl build for good

--- make.cmd
+++ make.cmd
@@ -1,5 +1,5 @@
 @echo OFF
-cl /nologo /LD /O2 /I include /DMG_ENABLE_MD5 /DMG_ENABLE_IPV6 /DMG_ENABLE_LINES /DMG_ENABLE_OPENSSL mongoose/mongoose.c -o mongoose.dll /link /DEF:mongoose.def Ws2_32.lib libssl.lib libcrypto.lib
+cl /nologo /LD /O2 /I include /DMG_ENABLE_MD5 /DMG_ENABLE_IPV6 /DMG_ENABLE_LINES /DMG_ENABLE_OPENSSL mongoose/mongoose.c -o mongoose.dll /link /DEF:mongoose.def WS2_32.LIB libssl.lib libcrypto.lib GDI32.LIB ADVAPI32.LIB CRYPT32.LIB USER32.LIB
 @REM cl /nologo /O2 mongoose/mongoose.c main.c /Fe"test-mongoose.exe" /link Ws2_32.lib
 @REM cl /nologo /O2 test.c /Fe"test.exe" /link
 REM cl /nologo /O2 utlist_test.c /Fe"utlist_test.exe" /link
